c6a71cf91757e819f370bd78fb0ba4d171d0ef24
Vidyashreeraksha <62463818+Vidyashreeraksha@users.noreply.github.com>
2020-09-02 19:29:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("search");
+});
+
+app.get("/results", function (req, res) {
+  var query = req.query.search;
+  var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.render("results", { data: data });
+    }
+  });
+});
+
+app.listen(3000);
+console.log("Server is running!!");
